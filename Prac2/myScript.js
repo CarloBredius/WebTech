f5d30e6638a410805b0aecbbb312f66c49c027ce
CarloBredius <c.bredius@live.nl>
2018-03-02 12:07:35 +0100

    Add Seperate map for prac2
    And try some javascript, doesn't really work for me yet

---
+++ Prac2/myScript.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
+
+function onTestClick(element, clr) {
+    element.style.color = clr;
+}
