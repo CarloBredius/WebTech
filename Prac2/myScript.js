d6f4a0232fb261162af6598e83b523abb8340124
CarloBredius <c.bredius@live.nl>
2018-03-07 14:06:07 +0100

    It works
    wrong quotes from copy pasta

--- Prac2/myScript.js
+++ Prac2/myScript.js
@@ -1,7 +1,4 @@
 // JavaScript source code
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
-
 function onTestClick(element, clr) {
     element.style.color = clr;
 }
