0ac3771681c20c9aaba9e3c156667064e476f441
CarloBredius <c.bredius@live.nl>
2018-03-27 19:39:10 +0200

    get html from server
    It was way easier than I thought

--- Prac3/main.js
+++ Prac3/main.js
@@ -3,40 +3,9 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+app.use("/", express.static(__dirname + "/html"));
 app.use("/media", express.static(__dirname + "/media"));
+app.use("/css", express.static(__dirname + "/css"));
 
-http.createServer(function (req, res) {
-    handleRequests(req, res);
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello group 56.\n');
-}).listen(8051, 'localhost');
-
-function handleRequests(req, res) {
-    switch (req) {
-        case "GET":
-            // respond met object en alle cases of dat object er is, of het mag etc.
-            // hier komt een <head> en een <body>
-            console.log(req.url);
-            break;
-        case "HEAD":
-            break;
-        case "POST":
-            break;
-        case "PUT":
-            break;
-        case "DELETE":
-            break;
-        case "CONNECT":
-            break;
-        case "OPTIONS":
-            break;
-        case "TRACE":
-            break;
-        default:
-    }
-}
-
-function handleResources(req, res) {
-
-}
+// Onderaan houden
+http.createServer(app).listen(8051, 'localhost');
